43f65183ef5b1e6d3960c0153f0b9d7b795fe97a
pags666 <pags40502@gmail.com>
2026-04-26 11:44:00 +0530

    Create export_to_sheets.py

---
+++ src/export_to_sheets.py
@@ -0,0 +1,39 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import os
+import json
+
+def push_to_sheet(df, sheet_name):
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    # 🔥 Load from GitHub Secret
+    creds_dict = json.loads(os.environ["GOOGLE"])
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        creds_dict, scope
+    )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_key("1le7tQxVkznMvphgOB2T0tGyzb_ByeaOHJ4R9E5piY_A")
+    worksheet = sheet.worksheet(sheet_name)
+
+    worksheet.clear()
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    rows = [["Stock", "Sentiment", "", "Updated At"]]
+
+    for _, row in df.iterrows():
+        rows.append([
+            row["ticker"],
+            row["sentiment_score"],
+            "",
+            now
+        ])
+
+    worksheet.update(rows)
