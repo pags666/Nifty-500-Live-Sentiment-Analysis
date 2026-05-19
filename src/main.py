636620ff24f56368cedd69c6579b9266e9c3efbb
pags666 <pags40502@gmail.com>
2026-05-19 12:48:38 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,13 +10,13 @@ import pandas as pd
 from loguru import logger
 from tqdm import tqdm
 
-import src.utils as utils
-from src.database import DatabaseManager
-from src.news_fetcher import TickerNewsObject
+import utils as utils
+from database import DatabaseManager
+from news_fetcher import TickerNewsObject
 from datetime import datetime, timedelta
-from src.export_to_sheets import push_to_sheet
+from export_to_sheets import push_to_sheet
 
-from src.utils import get_relative_date
+from utils import get_relative_date
 import pytz
 
 ist = pytz.timezone("Asia/Kolkata")
